f362b54c9d1169dfd4b0be7893e08c4451ad4229
AnabelSalomone <anabel.salomone@gmail.com>
2021-02-04 21:03:18 +0100

    wip + animation only turns when playing music

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <Song currentSong={currentSong}/>
+      <Song currentSong={currentSong} isPlaying={isPlaying}/>
       <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong}/>
     </div>
   );
--- src/components/Song.js
+++ src/components/Song.js
@@ -2,11 +2,11 @@ import React from "react";
 import "../styles/index.css";
 
 const Song = (props) => {
-  const { currentSong } = props;
+  const { currentSong, isPlaying } = props;
 
   return (
     <div className="song-container">
-      <img src={currentSong.cover} />
+      <img src={currentSong.cover} className={isPlaying ? `song-cover` : null} />
       <h2>{currentSong.name}</h2>
       <h3>{currentSong.artist}</h3>
     </div>
